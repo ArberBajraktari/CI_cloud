f9bad910a1ac96149b89a4b2b2b95b0d9e1ef60c
Arber Bajraktari <arberbajraktari@gmail.com>
2022-06-19 15:19:58 +0200

    conenction wroks

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    resp = requests.get('http://0.0.0.0:80/getData')
+    return 'Response from TestRequest: '+resp.text
 
 
 if __name__ == '__main__':
