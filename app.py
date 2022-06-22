7461864b5f2fc2353a8a421f1a19c6d83181c42e
Anis Shkembi <anis.shkembi@gmail.com>
2022-06-22 11:37:53 +0200

    Data is received.

--- app.py
+++ app.py
@@ -1,42 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for
-import time
-import requests
+import requests, json
+
 app = Flask(__name__)
-count = 0
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
-@app.route('/')
+
+def get_data():
+    data = None
+    try:
+        page = requests.get("http://localhost:80/")
+        data = page.text
+    except Exception as e:
+        print(e)
+        data = "No data available. Check your backend."
+    return data
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    output = "No request is made"
-    return render_template('home.html',output=output)
+    return render_template("home.html")
 
 
-@app.route('/<output>')
-def data_requested(output):
-    return render_template('home.html',output=output)
+@app.route("/get_data")
+def data_requested(data):
+    return render_template("home.html", data=data)
 
 
-@app.route('/handle_data', methods=['POST'])
+@app.route("/handle_data", methods=["POST"])
 def handle_data():
-    global count
-    page = ''
-    while page == '':
-        try:
-            page = requests.get('http://0.0.0.0:80/getData')
-            break
-        except:
-            print("Connection refused by the server..")
-            print("ZZzzzz...")
-            time.sleep(2)
-            print("Was a nice sleep, now let me continue...")
-            count += 1
-            if count == 3:
-                page = """The connection to the API could not be established.
-                Please check if the API is running!"""
-                break
-            continue
-    return redirect(url_for('data_requested', output=page))
+    data = get_data()
+    return render_template("home.html", data=data)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
